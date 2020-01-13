26d7066d8bfc276dd924b571a5dd264481b81a45
Henrique Valcanaia <henriqueindalencio@gmail.com>
2020-01-13 20:35:30 -0300

    Initial Commit

---
+++ Modal presentation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Modal presentation
+//
+//  Created by Henrique Valcanaia on 2020-01-13.
+//  Copyright © 2020 Henrique Valcanaia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
